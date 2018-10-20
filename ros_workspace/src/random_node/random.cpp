c2e0f6540552113e4278777909e0490dfe75c5f0
Jacob Dahl <jacob.h.dahl@hotmail.no>
2018-10-20 16:43:45 +0200

    idk

---
+++ ros_workspace/src/random_node/random.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include "ros/ros.h"
+#include "msg/Num.msg"
+
+
+
+int main(int argc, char **argv){
+
+    ros::init(argc,argv,"random");
+
+    ros::NodeHandle nh;
+
+    //ros::Publisher random_pub=nh.advertise<>
+
+}
+
+
+ 
+
+
